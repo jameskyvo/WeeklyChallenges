e143766f638660afcb9700ef9ee15bceab509de4
gamejunkiez <queenangrylobster@gmail.com>
2022-10-31 19:50:50 -0500

    completed challenges

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +8,110 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Contains(null))
+            {
+                return false;
+            }
+            foreach(var item in words)
+            {
+                if (ignoreCase == true)
+                {
+                    if (item.Equals(word, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    if (String.Equals(item,word))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if(num <= 1)
+            {
+                return false ;
+            }
+            for (int i = 2; i <= num/2; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            int index = -1;
+            bool uindex;
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                uindex = true;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[i] == str[j] && i != j)
+                    {
+                        uindex = false;
+                    }
+                }
+                if (uindex)
+                {
+                    index = i;
+                }
+            }
+            return index;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentCount = 1;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] != numbers[i])
+                    {
+                        break;
+                    }
+                currentCount++;
+                }
+                    if (currentCount > count)
+                    {
+                    count = currentCount;
+                    }  
+            }
+            return count;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            var nthElement = new List<double>();
+            double[] nullCheck = new double[0];
+            if (elements == null || n < 1)
+            {
+                return nullCheck;
+            }
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] % n == 0)
+                {
+                    nthElement.Add(elements[i]);
+                }
+            }
+            double[] finalArray = nthElement.ToArray();
+            return finalArray;
         }
     }
 }
